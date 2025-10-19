91413b8a819643320b6f3b75bdab9e4f0562e042
Godfather3500 <harpreerkm@gmail.com>
2025-10-19 14:07:27 +0530

    frontend vercel config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ import Shop from "./pages/Shop";
 import { useEffect } from "react";
 import { io } from "socket.io-client";
 import { setSocket } from "./redux/userSlice";
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "http://dash-dine.vercel.app/";
 
 function App() {
   const { userData } = useSelector((state) => state.user);
