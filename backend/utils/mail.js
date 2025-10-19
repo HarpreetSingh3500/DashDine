d1f24250cfb1a1324ba9964d0556749f1e6c6989
Godfather3500 <harpreerkm@gmail.com>
2025-10-19 19:34:44 +0530

    brevo

--- backend/utils/mail.js
+++ backend/utils/mail.js
@@ -1,34 +1,55 @@
-import nodemailer from "nodemailer";
+import * as SibApiV3Sdk from "@sendinblue/client";
 import dotenv from "dotenv";
 dotenv.config();
 
-// transporter config
-const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  port: 465,
-  secure: true, // true for 465, false for other ports
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+apiInstance.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
+
+const sender = {
+  email: process.env.EMAIL, // The "from" email you verified on Brevo
+  name: "Dash Dine",
+};
 
 // function to send otp to user's email from my(app) email
 export const sendOtpMail = async (to, otp) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL,
-    to,
-    subject: "Reset Your Password",
-    html: `<p>Your otp for reset DashDine password is <b>${otp}</b>. It expires in 5 minutes.</p>`,
-  });
+  const tranEmailApi = new SibApiV3Sdk.SendSmtpEmail();
+
+  tranEmailApi.sender = sender;
+  tranEmailApi.to = [{ email: to }];
+  tranEmailApi.subject = "Reset Your Password";
+  tranEmailApi.htmlContent = `<p>Your OTP for reset DashDine password is <b>${otp}</b>. It expires in 5 minutes.</p>`;
+
+  apiInstance.sendTransacEmail(tranEmailApi).then(
+    function (data) {
+      console.log(
+        "Password reset OTP seny successfully.Return data: " +
+          JSON.stringify(data)
+      );
+    },
+    function (error) {
+      console.error("Error sending password reset OTP:", error);
+    }
+  );
 };
 
 // function to send otp to user's email from my(app) email
 export const sendDeliveryOtpMail = async (user, otp) => {
-  await transporter.sendMail({
-    from: process.env.EMAIL,
-    to: user.email,
-    subject: "Delivery OTP",
-    html: `<p>Your otp for delivery confirmation is <b>${otp}</b>. It expires in 5 minutes.</p>`,
-  });
+  const tranEmailApi = new SibApiV3Sdk.SendSmtpEmail();
+
+  tranEmailApi.sender = sender;
+  tranEmailApi.to = [{ email: user.email }];
+  tranEmailApi.subject = "Delivery OTP";
+  tranEmailApi.htmlContent = `<p>Your otp for delivery confirmation is <b>${otp}</b>. It expires in 5 minutes.</p>`;
+
+  apiInstance.sendTransacEmail(tranEmailApi).then(
+    function (data) {
+      console.log(
+        "Delivery OTP sent successfully. Returned data: " + JSON.stringify(data)
+      );
+    },
+    function (error) {
+      console.error("Error sending delivery OTP:", error);
+    }
+  );
 };
